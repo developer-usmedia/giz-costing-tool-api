e1bfb7df62447585bda443d5ddf904d7ba487634
Koen Lippe <koen.lippe@usmedia.nl>
2024-03-11 13:47:05 +0000

    feature/GIZ-78: Allow custon benchmark saving
    Merged in feature/giz-78-entry-allow-benchmark-saving (pull request #29)
    
    * feat: allow custom benchmark saving via simulation patch
    
    * feat: add curcurrencyCode to saving

--- src/api/simulation/dto/update-simulation.form.ts
+++ src/api/simulation/dto/update-simulation.form.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsObject, IsOptional, ValidateNested } from 'class-validator';
 
 import { Simulation } from '@database/entities';
+import { SimulationBenchmarkForm } from './simulation-benchmark.form';
 import { SimulationFacilityForm } from './simulation-facility.form';
 
 /**
@@ -34,6 +35,12 @@ export class UpdateSimulationForm {
     @ValidateNested({ each: true })
     facility?: SimulationFacilityForm;
 
+    @ApiProperty({ nullable: true })
+    @IsObject()
+    @IsOptional()
+    @ValidateNested({ each: true })
+    benchmark?: SimulationBenchmarkForm;
+
     // Convert to database entity from DTO specified above
     public static toEntity(simulation: Simulation, form: UpdateSimulationForm): Simulation {
         if (form.year) simulation.year = form.year;
@@ -42,6 +49,7 @@ export class UpdateSimulationForm {
         if (form.defaultEmployeeTax) simulation.defaultEmployeeTax = form.defaultEmployeeTax;
 
         if (form.facility) simulation.facility = SimulationFacilityForm.toEntity(simulation.facility, form.facility);
+        if (form.benchmark) simulation.benchmark = SimulationBenchmarkForm.toEntity(simulation.benchmark, form.benchmark, simulation.facility);
 
         return simulation;
     }
