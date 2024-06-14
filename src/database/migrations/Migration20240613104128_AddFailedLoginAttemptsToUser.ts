b7e71bef596d4b570dc490cb5725e0f1c773f4db
Koen Lippe <koen.lippe@usmedia.nl>
2024-06-14 13:52:16 +0200

    fix: code review comment

--- src/database/migrations/Migration20240613104128_AddFailedLoginAttemptsToUser.ts
+++ src/database/migrations/Migration20240613104128_AddFailedLoginAttemptsToUser.ts
@@ -3,7 +3,8 @@ import { Migration } from '@mikro-orm/migrations';
 export class Migration20240613104128_AddFailedLoginAttemptsToUser extends Migration {
 
   async up(): Promise<void> {
-    this.addSql('alter table "giz_user" add column "failed_login_attempts" integer not null default 0, add column "failed_login_lock_until" timestamp null;');
+    this.addSql('alter table "giz_user" add column "failed_login_attempts" integer not null default 0;');
+    this.addSql('alter table "giz_user" add column "failed_login_lock_until" timestamp null;');
   }
 
   async down(): Promise<void> {
