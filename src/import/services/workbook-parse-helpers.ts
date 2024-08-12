50c16f4d12a8810e559d8976669d55887a35268c
Koen Lippe <koen.lippe@usmedia.nl>
2024-08-12 12:36:47 +0200

    GIZ-268: add other translations for gender

--- src/import/services/workbook-parse-helpers.ts
+++ src/import/services/workbook-parse-helpers.ts
@@ -2,9 +2,17 @@ import { Gender } from '@domain/entities';
 
 export function parseGenderCell(value: string): Gender {
     switch (value) {
-        case 'Men':
+        case 'Men':         // EN
+        case 'Hombres':     // ES
+        case 'Hommes':      // FR
+        case 'Homens':      // PT
+        case 'Nam':         // VN
             return 'MEN';
-        case 'Women':
+        case 'Women':       // EN
+        case 'Mujeres':     // ES
+        case 'Femmes':      // FR
+        case 'Mulheres':    // PT
+        case 'Nữ':          // VN
             return 'WOMEN';
         default:
             return null;
