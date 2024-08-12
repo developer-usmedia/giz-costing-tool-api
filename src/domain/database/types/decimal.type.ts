2f695f95b4cf9de6044fc9d65c54f6f57202b603
Koen Lippe <koen.lippe@usmedia.nl>
2024-08-12 11:23:18 +0200

    fix: sonar unreachable code

--- src/domain/database/types/decimal.type.ts
+++ src/domain/database/types/decimal.type.ts
@@ -1,30 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import {
-  EntityProperty,
-  Platform,
-  Type,
-  ValidationError,
-} from '@mikro-orm/core';
+import { EntityProperty, Platform, Type } from '@mikro-orm/core';
 import Decimal from 'decimal.js';
 
 export class DecimalType extends Type<Decimal, string> {
-  convertToDatabaseValue(value: Decimal, _platform: Platform): string {
-    if (value === null) return null;
-    if (value.isNaN() || !value.isFinite()) {
-      return null;
-      throw ValidationError.invalidType(DecimalType, value, 'JS');
+    convertToDatabaseValue(value: Decimal, _platform: Platform): string {
+        if (value === null) return null;
+        if (value.isNaN() || !value.isFinite()) {
+            return null;
+        }
+
+        return value.toFixed(5);
     }
 
-    return value.toFixed(5);
-  }
+    convertToJSValue(value: string, _platform: Platform): Decimal {
+        if (value === null) return null;
 
-  convertToJSValue(value: string, _platform: Platform): Decimal {
-    if (value === null) return null;
-    
-    return new Decimal(value);
-  }
+        return new Decimal(value);
+    }
 
-  getColumnType(_prop: EntityProperty, _platform: Platform) {
-    return 'numeric';
-  }
+    getColumnType(_prop: EntityProperty, _platform: Platform) {
+        return 'numeric';
+    }
 }
